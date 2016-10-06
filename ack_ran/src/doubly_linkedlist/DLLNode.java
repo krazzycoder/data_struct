ea9d5507dd42e8e171b84eadfecba5793069171a
krazzycoder <practcode@gmail.com>
2016-10-07 04:43:47 +0530

    DLL_init

---
+++ ack_ran/src/doubly_linkedlist/DLLNode.java
@@ -0,0 +1,53 @@
+package doubly_linkedlist;
+
+public class DLLNode 
+{
+	private int data;
+	private DLLNode prev;
+	private DLLNode next;
+	
+	public DLLNode(int data)
+	{
+		this.data=data;
+		prev=null;
+		next=null;
+	}
+	
+	public DLLNode(int data,DLLNode next,DLLNode prev)
+	{
+		this.data=data;
+		this.prev=prev;
+		this.next=next;
+	}
+	
+	public int getData()
+	{
+		return data;
+	}
+	
+	public void setData(int data)
+	{
+		this.data=data;
+	}
+	
+	public DLLNode getPrev()
+	{
+		return prev;
+	}
+	
+	public DLLNode getNext()
+	{
+		return next;
+	}
+	
+	public void setPrev(DLLNode where)
+	{
+		this.prev=where;
+	}
+	
+	public void setNext(DLLNode where)
+	{
+		this.next=where;
+	}
+	
+}
