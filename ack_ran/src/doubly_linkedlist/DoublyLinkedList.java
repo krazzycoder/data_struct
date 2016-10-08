a4248df55a0c34cd3c2c24a72d1985daf149ecd9
krazzycoder <practcode@gmail.com>
2016-10-09 03:17:14 +0530

    commit2

--- ack_ran/src/doubly_linkedlist/DoublyLinkedList.java
+++ ack_ran/src/doubly_linkedlist/DoublyLinkedList.java
@@ -29,29 +29,38 @@ public class DoublyLinkedList
 	
 	public void insert(int data,int position)
 	{
+	//	System.out.println("data="+data+" position="+position);
 		if(position<0)
 			position=0;
 		if(position>length)
 			position=length-1;
 		
+	//	System.out.println("data="+data+" position="+position);
 		if(head.getNext()==tail)
 		{
 			DLLNode dll=new DLLNode(data,head,tail);
 			head.setNext(dll);
 			tail.setPrev(dll);
+			System.out.println("going heer 1");
 		}
 		else if(position==0)
 		{
 			DLLNode newnode=new DLLNode(data,head,head.getNext());
 			head.getNext().setPrev(newnode);
 		    head.setNext(newnode);
+			System.out.println("going heer 2");
 		    
 		}
 		else 
 		{
+		//	System.out.println("going here 3 position="+position);
 			DLLNode temp=head.getNext();
 			for(int i=1;i<position;i++)
+			{
 				temp=temp.getNext();
+				//System.out.print(i+" times");
+			}
+			//System.out.println();
 			
 			DLLNode newnode=new DLLNode(data,temp,temp.getNext());
 			temp.getNext().setPrev(newnode);
@@ -86,6 +95,43 @@ public class DoublyLinkedList
 		    tail.setPrev(newnode);
 		}
 		
+		length++;
+		
+	}
+	
+	public int removeHead()
+	{
+		if(length==0)
+			return Integer.MIN_VALUE;
+		
+	    DLLNode save=head.getNext();
+	    head.setNext(save.getNext());
+	    save.getNext().setPrev(head);
+	    length--;
+	    return save.getData();
+	}
+	
+	public int removeTail()
+	{
+		if(length==0)
+			return Integer.MIN_VALUE;
+		
+		DLLNode node=tail.getPrev();
+		tail.setPrev(node.getPrev());
+		node.getPrev().setNext(tail);
+		length--;
+		return node.getData();
+		
+	}
+	
+	public void remove(int position)
+	{
+		
+	}
+	
+	public void removeMatched(int data)
+	{
+		
 	}
 	
 	public String toString()
@@ -104,6 +150,13 @@ public class DoublyLinkedList
 		return strbuilder.toString();
 	}
 	
+	
+	public void clear()
+	{
+		head.setNext(tail);
+		tail.setPrev(head);
+		length=0;
+	}
 	
 	
 	
